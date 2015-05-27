299486ae2da27890455b35bd86393a6d69696b4d
allen wes <allen.wes.g@gmail.com>
2015-05-27 09:59:42 -0500

    minor JS stuff

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
 
+	$username = $("#username");
+	$password = $("#password");
+	$form = $("#login");
+
+	$form.on("submit", validateInfo);
+
+	function validateInfo(event){
+		event.preventDefault();
+		console.log("i work!");
+	}
 	
 });
